64bff9da3e9251cbcef0795de4cf90577ebec024
Benjamin Jensen <ben@tamam.dk>
2023-02-14 13:19:50 +0100

    Added leds module

---
+++ MIDI_Commander_Custom/Core/Inc/leds.h
@@ -0,0 +1,13 @@
+/*
+ * leds.h
+ *
+ *  Created on: Feb 14, 2023
+ *      Author: ben
+ */
+
+#ifndef INC_LEDS_H_
+#define INC_LEDS_H_
+
+
+
+#endif /* INC_LEDS_H_ */
---
+++ MIDI_Commander_Custom/Core/Src/leds.c
@@ -0,0 +1,8 @@
+/*
+ * leds.c
+ *
+ *  Created on: Feb 14, 2023
+ *      Author: ben
+ */
+
+#include "leds.h"
